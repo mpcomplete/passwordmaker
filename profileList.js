793b698f8a604ce613f19854b4cb9d538f9791aa
Matt Perry <mpcomplete@google.com>
2012-05-18 15:37:03 -0700

    oops.js.files

---
+++ profileList.js
@@ -0,0 +1,30 @@
+/*
+profileLB = new Bs_Dropdown();
+        profileLB.imgDir = 'scripts/blueShoes/components/dropdown/img/win2k/';
+profileLB.setValue('Default');
+profileLB.drawInto('profileLB');
+profileLB.attachEvent('onChange', loadProfile);
+*/
+
+if(getCookie("profileList")!=null) {
+        // load the various profiles
+        var a = unescape(getCookie("profileList"));
+        var profileListArray = a.split("|");
+
+        for (var i=0; i<profileListArray.length; i++) {
+                var option = document.createElement("option");
+                option.text = unescape(profileListArray[i]);
+                EditableSelect.selectAddOption(document.getElementById("profileLB"), option);
+
+                if(i==0)
+                        option.selected="selected";
+        }
+}
+else {
+        profileListArray = new Array("Default");
+
+        var option = document.createElement("option");
+        option.text = "Default";
+        EditableSelect.selectAddOption(document.getElementById("profileLB"), option);
+        option.selected="selected";
+}
